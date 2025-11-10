584f643305d5cf8279a27d9dc7034288cc54c6de
DanielHenriqueBezerra <danielxt499@gmail.com>
2025-11-10 19:26:25 -0300

    Apagando

--- React/primeiro_app/src/Componentes/Compo.js
+++
@@ -1,9 +0,0 @@
-function Comp() {
-  return (
-    <div>
-      <h2>Este é um componente chamado Compo</h2>
-    </div>
-  );
-}
-
-export default Comp;
--- React/sexta-feira/src/Componentes/jovemtech.js
+++
@@ -1,9 +0,0 @@
-function Jovemtech ({nome, idade}) {
-  return (
-    <div>
-      <h1>A empresa que irei trabalha é a: {nome} com apenas {idade}!</h1>
-    </div>
-  );
-}
-
-export default Jovemtech;
